ed58a0c83983a5722f9b384fde835d88dfeb55dc
milesmorales365a-creator <milesmorales365a@gmail.com>
2026-08-24 14:04:26 +0300

    Update app.py for Heroku deployment with proper port configuration

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import random
+import os
 
 app = Flask(__name__)
 
@@ -72,4 +73,5 @@ def check_answer():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
